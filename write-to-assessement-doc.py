c060592a5cb418e6f435bf50e75ab67be8e560f7
christopherKrug <33500639+christopherKrug@users.noreply.github.com>
2017-11-23 10:33:20 -0600

    Update write-to-assessement-doc.py

--- write-to-assessement-doc.py
+++ write-to-assessement-doc.py
@@ -1,1 +1,11 @@
 def write_to_assessment_doc():
+    fileName = site+"_"+assessment+"_"+timestamp+'.docx'
+    if os.path.isfile(fileName):
+        assessmentDoc = docx.Document(fileName)
+        assessmentDoc.add_heading(assessment, level=1) # (()
+        assessmentDoc.add_paragraph("to fill")
+        assessmentDoc.save(fileName)
+    else:
+        assessmentDoc = docx.Document()
+        assessmentDoc.add_heading(site+" "+timestamp,0)
+        assessmentDoc.save(fileName)      
